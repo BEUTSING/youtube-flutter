0cff1b38dce0b6b584b800f22ec85b8772dc6ba5
BEUTSING <beutsingjeanne762@gmail.com>
2025-11-07 08:50:15 +0100

    Add Windows runner files for Flutter application
    - Created runner.exe.manifest to define application settings and DPI awareness.
    - Implemented utils.cpp and utils.h for console creation and command line argument handling.
    - Developed win32_window.cpp and win32_window.h to manage a high DPI-aware Win32 window, including theme updates and message handling.

---
+++ cours_dart/premier_programme/bin/main.dart
@@ -0,0 +1,38 @@
+class User {
+  int id;
+  String name;
+  String lastname;
+  int age;
+  String sexe;
+  User(this.id, this.name, this.lastname, this.age, this.sexe);
+  void afficher() {
+    print('ID: $id, name: $name, lastname: $lastname, Âge: $age, Sexe: $sexe');
+  }
+}
+
+void main() {
+  List<User> users = [
+    User(1, 'name', 'lastname', 20, 'f'),
+    User(2, 'name', 'moi', 27, ' m'),
+    User(3, 'name', 'lastname', 52, 'm'),
+    User(4, 'name', 'lastname', 27, 'm'),
+  ];
+  //supression de l'user avec l'id 30
+  for (var user in users) {
+    if (user.id == 30) {
+      users.remove(user);
+    }
+  }
+  //recherche de l'user avec l'id 2
+  var user2 = users.firstWhere((u) => u.id == 2);
+  user2.afficher();
+  //creation d'un nouvel user
+  var newUser=User(340,'voko','william',50,'m');
+  users.insert(2,newUser);
+
+  //affichage de tous les users
+
+  for (var user in users) {
+    user.afficher();
+  }
+}
